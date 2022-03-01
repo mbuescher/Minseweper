0be40d8bb7e30cf5cecea74140f9d98960e07205
CHRISTINABENCIN <89540732+CHRISTINABENCIN@users.noreply.github.com>
2022-03-01 12:05:38 -0500

    Update MinseweperGame.java

--- MinseweperGame.java
+++ MinseweperGame.java
@@ -84,6 +84,133 @@ public class MinseweperGame extends JPanel
                     buttonClicked.setText("");
                 } */
             }
+            
+            public int getNumber(int r, int c)
+    {
+        int count = 0; 
+
+        if (r>0 && c>0 && r<NUMROWS-1 && c<NUMCOLS-1)//if it is in the middle of the grid
+        {
+            for(int i = r-1; i<=r+1; i++)
+            {
+                for (int j = c-1; j<=c+1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        else if(r==0 && c==0)//check the top left corner
+        {
+            for (int i = 0; i<=r+1; i++)
+            {
+                for (int j = 0; j<=c+1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        else if(r==NUMROWS-1 && c==0)//check the bottom left corner
+        {
+            for (int i = NUMROWS-2; i<=NUMROWS-1; i++)
+            {
+                for (int j = 0; j<=c+1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        else if(r==0 && c==NUMCOLS-1)//check the top right corner
+        {
+            for (int i = 0; i<=r+1; i++)
+            {
+                for (int j = NUMCOLS-2; j<=NUMCOLS-1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        else if(r==NUMROWS-1 && c==NUMCOLS-1)//check the top left corner
+        {
+            for (int i = NUMROWS-2; i<=NUMROWS-1; i++)
+            {
+                for (int j = NUMCOLS-2; j<=NUMCOLS-1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        else if(r==0)//top edge
+        {
+            for (int i = 0; i<=1; i++)
+            {
+                for (int j = c-1; j<= c+1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        else if(r==NUMROWS-1)//bottom edge
+        {
+            for (int i = NUMROWS-2; i<=NUMROWS-1; i++)
+            {
+                for (int j = c-1; j<= c+1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        else if (c==0)//left side edge
+        {
+            for (int i = r-1; i<=r+1; i++)
+            {
+                for (int j =0; j<=1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        else if (c== NUMCOLS-1)
+        {
+            for (int i = r-1; i<=r+1; i++)
+            {
+                for (int j = NUMCOLS-2; j<=NUMCOLS-1; j++)
+                {
+                    if (grid[i][j].getValue()==-1)
+                    {
+                        count++;
+                    }
+                }
+            }
+            
+        }
+        
+        return count;
+    }
         }
         
         // Unused methods from the MouseListener interface
